6a59db712ceb3d911a18ef861287059146b9649c
Lexxie <pocktram@gmail.com>
2018-10-27 22:02:27 -0600

    Update search default selection to none

--- src/components/Search.js
+++ src/components/Search.js
@@ -58,10 +58,10 @@ class Search extends Component {
                       value={book.shelf}
                       onChange={(e) => this.props.onChangeShelf(book, e.target.value)}>
                       <option value="move" disabled>Move to...</option>
+                      <option value="none">None</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
                       <option value="read">Read</option>
-                      <option value="none">None</option>
                     </select>
                   </div>
                 </div>
